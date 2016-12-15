005f88ed5c4fcaed3046c48f7acdad4b543aa314
KevinWyso <kevin.wyso@gmail.com>
2016-12-15 11:49:26 -0500

    UI Update
    -Basic towers can be selected using 1-4 Number Keys or Numpad 1-4

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Shop : MonoBehaviour {
 
@@ -26,6 +28,18 @@ public class Shop : MonoBehaviour {
 			}
 			t.setCostLabel ();
 		}
+
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad0))
+			selectTurretTower();
+		else if(Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad1))
+			selectRocketTower();
+		else if(Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad2))
+			selectLaserTower();
+		else if(Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad3))
+			selectSniperTower();
 
 	}
 
